ade9976f5b64fa24abdab7b23d63ce6a1d013543
fabtasticwill <123williammorris@gmail.om>
2014-12-01 06:52:29 -0500

    Removed File class Completely

--- sandbox.rb
+++ sandbox.rb
@@ -13,12 +13,6 @@ end
 def remove(a)
     say_to_chan("I'm sorry Dave, I'm afraid I can't do that.")
 end
-class File
-	def open(a)
-		say_to_chan("I'm sorry Dave, I'm afraid I can't do that.")
-	end
-	def readlines(a)
-		say_to_chan("I'm sorry Dave, I'm afraid I can't do that.")
-	end
+class Object
+	remove_const :File
 end
-
